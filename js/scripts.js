9586752b5768a85616bc056f099934a852385a7c
aaron gershenzon and James Turello <aarongershenzon@gmail.com>
2016-01-13 16:56:42 -0800

    tried to add if logic to name and email fields, began working on radios

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,33 @@
 $(document).ready(function() {
+  $("form#date").submit(function() {
 
+    var nameok = false;
+    var emailok = false;
+
+    var name = $("input#name").val();
+    var email = $("input#email").val();
+
+    if (name) {
+      nameok = true;
+    }
+
+    if(email) {
+      emailok = true;
+    }
+
+    if (nameok && emailok) {
+      // alert("if: nameok=" + nameok + ", emailok=" + emailok);
+    } else {
+      alert("else: nameok=" + nameok + ", emailok=" + emailok);
+
+    }
+
+    if ($('input[id="#male"]:checked').val()) {
+      alert("male");
+    } else if ($('input[id="#female"]:checked').val());{
+      alert("female");
+    } else if ($('input[name="gender",id="#genderneutral"]:checked').val()) {
+      alert("neutral")
+    }
+  });
 });
